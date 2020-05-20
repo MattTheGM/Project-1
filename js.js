3478f0294a1ef2822b89607ba51d2943f9282fb4
Matt <matthewjmcivor@gmail.com>
2020-05-19 19:26:55 -0500

    Commented out template buttons, finalized function

--- js.js
+++ js.js
@@ -68,8 +68,42 @@ $( document ).ready(function() {
         initMap();
     });
 
+    // $('#pyramid-button').click(function pyramidMap() {
+    //     coords =  {
+    //         lat: 29.9792,
+    //         lng: 31.1342
+    //     };
+    //     returnWeatherCoords(coords.lat, coords.lng);
+    //     initMap();
+    // });
+
+    // $('#pyramid-button').click(function pyramidMap() {
+    //     coords =  {
+    //         lat: 29.9792,
+    //         lng: 31.1342
+    //     };
+    //     returnWeatherCoords(coords.lat, coords.lng);
+    //     initMap();
+    // });
+
+    // $('#pyramid-button').click(function pyramidMap() {
+    //     coords =  {
+    //         lat: 29.9792,
+    //         lng: 31.1342
+    //     };
+    //     returnWeatherCoords(coords.lat, coords.lng);
+    //     initMap();
+    // });
+
+    if ("input url uses coords") {
+        
+    } else if ("input url uses city") {
+
+    } else { 
+
+    };
+
     function returnWeatherCoords(arg1,arg2) {
-        // var latLongQuery = "api.openweathermap.org/data/2.5/weather?lat=29.9792&lon=31.1342&appid=db417286ffd067d079c3760d5405b45d";
         var latLongQuery = "https://api.openweathermap.org/data/2.5/weather?" + "lat=" + arg1 + "&lon=" + arg2 + "&appid=" + APIKey;
         var latLongQuery2 = "https://api.openweathermap.org/data/2.5/forecast?" + "lat=" + arg1 + "&lon=" + arg2 + "&appid=" + APIKey;
         $.ajax({    
@@ -81,7 +115,6 @@ $( document ).ready(function() {
             $(".today-wind").text("Wind Speed: " + response.wind.speed + " MPH");
             $(".today-humidity").text("Humidity: " + response.main.humidity + " %");
 
-        // Display Weather Icons
         // Icon sample => <i class="owf owf-200"></i>
             $("#display-icon").html("<i class='owf owf-" + response.weather[0].id + " owf-5x'" + "></i>");
             // // Convert the temp to fahrenheit    
@@ -90,66 +123,61 @@ $( document ).ready(function() {
             $(".today-temp").text("Temperature (K) " + response.main.temp);
             $(".today-tempF").text("Temperature: " + tempF.toFixed(2) + " (°F)");
             // First 4 days
-            $.ajax({    
-                url: latLongQuery2,      
-                method: "GET"
-            })
-                .then(function(response) {
-    
-                // Log the queryURL
-    
-                // Log the resulting object
-    
-                // // Transfer content to HTML
-                for (i = 0; i <= 40; i += 8) {
-    
-                    $("#wind-"+[i]).text("Wind Speed: " + response.list[i].wind.speed + " MPH");
-                    $("#humidity-"+[i]).text("Humidity: " + response.list[i].main.humidity + " %");
-    
-                    // Display Weather Icons
-                    $("#show-me-"+[i]).html("<i class='owf owf-" + response.list[i].weather[0].id + " owf-3x'" + "></i>");
-    
-                    // // Convert the temp to fahrenheit    
-                    var tempF = (response.list[i].main.temp - 273.15) * 1.80 + 32;
-                    console.log("Day: " + [i] + " Temp: " + tempF);
-    
-                    // add temp content to html
-                    $(".temp").text("Temperature (K) " + response.list[i].main.temp);
-                    $("#tempF-"+[i]).text("Temperature: " + tempF.toFixed(2) + " (°F)");
-    
-                    // Log the data in the console as well
-                    console.log("Wind Speed: " + response.list[i].wind.speed);
-                    console.log("Humidity: " + response.list[i].main.humidity);
-                    console.log("Temperature (F): " + tempF);
-                };
-            });
-            //Last Day API Call
-            $.ajax({    
-                url: latLongQuery2,      
-                method: "GET"
-            })
-                .then(function(response) {
-    
-                // // Transfer content to HTML
-                $("#wind-"+[39]).text("Wind Speed: " + response.list[39].wind.speed + " MPH");
-                $("#humidity-"+[39]).text("Humidity: " + response.list[39].main.humidity + " %");
-    
-                // Display Weather Icons
-                $("#show-me-"+[39]).html("<i class='owf owf-" + response.list[39].weather[0].id + " owf-3x'" + "></i>");
-    
-                // // Convert the temp to fahrenheit    
-                var tempF = (response.list[39].main.temp - 273.15) * 1.80 + 32;
-                console.log("Day: " + [39] + " Temp: " + tempF);
-    
-                // add temp content to html
-                $(".temp").text("Temperature (K) " + response.list[39].main.temp);
-                $("#tempF-"+[39]).text("Temperature: " + tempF.toFixed(2) + " (°F)");
-    
-                // Log the data in the console as well
-                console.log("Wind Speed: " + response.list[39].wind.speed);
-                console.log("Humidity: " + response.list[39].main.humidity);
-                console.log("Temperature (F): " + tempF);
-            });
+        });
+        $.ajax({    
+            url: latLongQuery2,      
+            method: "GET"
+        })
+        .then(function(response) {
+
+        for (i = 0; i <= 40; i += 8) {
+
+            $("#wind-"+[i]).text("Wind Speed: " + response.list[i].wind.speed + " MPH");
+            $("#humidity-"+[i]).text("Humidity: " + response.list[i].main.humidity + " %");
+
+            // Display Weather Icons
+            $("#show-me-"+[i]).html("<i class='owf owf-" + response.list[i].weather[0].id + " owf-3x'" + "></i>");
+
+            // // Convert the temp to fahrenheit    
+            var tempF = (response.list[i].main.temp - 273.15) * 1.80 + 32;
+            console.log("Day: " + [i] + " Temp: " + tempF);
+
+            // add temp content to html
+            $(".temp").text("Temperature (K) " + response.list[i].main.temp);
+            $("#tempF-"+[i]).text("Temperature: " + tempF.toFixed(2) + " (°F)");
+
+            // Log the data in the console as well
+            console.log("Wind Speed: " + response.list[i].wind.speed);
+            console.log("Humidity: " + response.list[i].main.humidity);
+            console.log("Temperature (F): " + tempF);
+        };
+        });
+        //Last Day API Call
+        $.ajax({    
+            url: latLongQuery2,      
+            method: "GET"
+        })
+            .then(function(response) {
+
+            // // Transfer content to HTML
+            $("#wind-"+[39]).text("Wind Speed: " + response.list[39].wind.speed + " MPH");
+            $("#humidity-"+[39]).text("Humidity: " + response.list[39].main.humidity + " %");
+
+            // Display Weather Icons
+            $("#show-me-"+[39]).html("<i class='owf owf-" + response.list[39].weather[0].id + " owf-3x'" + "></i>");
+
+            // // Convert the temp to fahrenheit    
+            var tempF = (response.list[39].main.temp - 273.15) * 1.80 + 32;
+            console.log("Day: " + [39] + " Temp: " + tempF);
+
+            // add temp content to html
+            $(".temp").text("Temperature (K) " + response.list[39].main.temp);
+            $("#tempF-"+[39]).text("Temperature: " + tempF.toFixed(2) + " (°F)");
+
+            // Log the data in the console as well
+            console.log("Wind Speed: " + response.list[39].wind.speed);
+            console.log("Humidity: " + response.list[39].main.humidity);
+            console.log("Temperature (F): " + tempF);
         });
     }
 
