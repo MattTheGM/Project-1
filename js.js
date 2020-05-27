65d0859b1a7148af14f2acfdfabcb468b66b047e
Miguel Mendoza <mendoza17miguel@gmail.com>
2020-05-27 16:59:11 -0500

    Added modal

--- js.js
+++ js.js
@@ -252,22 +252,6 @@ function forecastWeather () {
     });
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 $( document ).ready(function() {
     const searchButton = document.querySelector('.search-button');
     const searchList = document.querySelector('.search-list');
@@ -461,6 +445,26 @@ $( document ).ready(function() {
         inputValue.value = "";
     });
 });
+
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 
 //     $.ajax({    
