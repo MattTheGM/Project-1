a8bb369407e16f47daf802d1a636cf4ffd660caa
Matt <matthewjmcivor@gmail.com>
2020-05-28 18:28:17 -0500

    Finalized Version of Code

--- buttonfunctions.js
+++ buttonfunctions.js
@@ -20,7 +20,7 @@ function nocheCard() {
             </p>
             <br>
             <p>
-                <strong>Wikipedia Page:</strong> https://en.wikipedia.org/wiki/La_Noche_Triste
+                <strong>Wikipedia Page: </strong><a href = "https://en.wikipedia.org/wiki/La_Noche_Triste" target="_blank">https://en.wikipedia.org/wiki/La_Noche_Triste</a>
             </p>
             <br>
             <p>
@@ -46,27 +46,27 @@ function pyramidCard() {
     <div class="back">
         <div class="back-content">
             <h2>
-                <strong>The Treasure of La Noche Triste</strong>
+                <strong>Treasure within Egyptian Pyramids</strong>
             </h2>
             <br>
             <p>
-                In June 1520, the Aztecs drove Hernan Cortes and the Spanish conquistadores out of the capital city of Tenochitlan. As the Spanish fled, they took with them vast amounts of Aztecan gold and treasure, much of which was lost during their escape in the depths of the Tenochitlan waterways.
+                In the times of the 3rd Dynasty in Ancient Egypt, pharaohs – the ruling god-emperors of the time – were typically buried in large structures called pyramids. The Ancient Egyptians believed that anything that you were buried with was sent with you into the afterlife. For this reason, pharaohs were traditionally buried with many treasures to help them settle comfortably into the afterlife. The treasure that lay within the pyramids of Egypt have entranced many archaeologists and treasure-hunters alike.
             </p>
             <br>
             <p>
-                <strong>Suspected Location:</strong> Waters around Tenochitlan (Dried Basin Under Mexico City)
+                <strong>Suspected Location:</strong> Greater Egypt
             </p>
             <br>
             <p>
-                <strong>Wikipedia Page:</strong> https://en.wikipedia.org/wiki/La_Noche_Triste
+                <strong>Wikipedia Page: </strong><a href = "https://en.wikipedia.org/wiki/Egyptian_pyramids" target="_blank">https://en.wikipedia.org/wiki/Egyptian_pyramids</a>
             </p>
             <br>
             <p>
-                <strong>Estimated Value:</strong> $500 Million
+                <strong>Estimated Value:</strong> Variable
             </p>
             <br>
             <p>
-                <strong>Origin:</strong> Aztec
+                <strong>Origin:</strong> Ancient Egyptian
             </p>
         </div>
     </div>
@@ -84,27 +84,27 @@ function nuestraCard() {
     <div class="back">
         <div class="back-content">
             <h2>
-                <strong>The Treasure of La Noche Triste</strong>
+                <strong>The Treasure of the Nuestra Señora</strong>
             </h2>
             <br>
             <p>
-                In June 1520, the Aztecs drove Hernan Cortes and the Spanish conquistadores out of the capital city of Tenochitlan. As the Spanish fled, they took with them vast amounts of Aztecan gold and treasure, much of which was lost during their escape in the depths of the Tenochitlan waterways.
+                Nuestra Señora de Atocha (Spanish: Our Lady of Atocha) was a Spanish treasure galleon and the most widely known vessel of a fleet of ships that sank in a hurricane off the Florida Keys in 1622. At the time of her sinking, Nuestra Señora de Atocha was heavily laden with copper, silver, gold, tobacco, gems, and indigo from Spanish ports at Cartagena and Porto Bello in New Granada (present-day Colombia and Panama, respectively) and Havana, bound for Spain. The ship was named for the parish of Atocha in Madrid. While most of the treasure was discovered in 1985, some part of its remains still lay unknown beneath the waters.
             </p>
             <br>
             <p>
-                <strong>Suspected Location:</strong> Waters around Tenochitlan (Dried Basin Under Mexico City)
+                <strong>Suspected Location:</strong> Key West Islands, Florida
             </p>
             <br>
             <p>
-                <strong>Wikipedia Page:</strong> https://en.wikipedia.org/wiki/La_Noche_Triste
+                <strong>Wikipedia Page: </strong><a href = "https://en.wikipedia.org/wiki/Nuestra_Se%C3%B1ora_de_Atocha" target="_blank">https://en.wikipedia.org/wiki/Nuestra_Se%C3%B1ora_de_Atocha</a>
             </p>
             <br>
             <p>
-                <strong>Estimated Value:</strong> $500 Million
+                <strong>Estimated Value:</strong> $200 Million
             </p>
             <br>
             <p>
-                <strong>Origin:</strong> Aztec
+                <strong>Origin:</strong> Spanish/Portuguese
             </p>
         </div>
     </div>
@@ -122,27 +122,27 @@ function natziCard() {
     <div class="back">
         <div class="back-content">
             <h2>
-                <strong>The Treasure of La Noche Triste</strong>
+                <strong>Lake Toplitz Treausre</strong>
             </h2>
             <br>
             <p>
-                In June 1520, the Aztecs drove Hernan Cortes and the Spanish conquistadores out of the capital city of Tenochitlan. As the Spanish fled, they took with them vast amounts of Aztecan gold and treasure, much of which was lost during their escape in the depths of the Tenochitlan waterways.
+                During WWII, Nazi Germany was a very militarized country. The state itself required numerous funds to manage to continue the war engine, and the state found itself starting to dry out its war chest rather early into the war. To deal with this, the Nazi’s began to loot and raid gold from conquered countries. While much of the gold was recovered, some of it was lost. A portion of this lost treasure is said to reside at the bottom of Lake Toplitz in Austria.
             </p>
             <br>
             <p>
-                <strong>Suspected Location:</strong> Waters around Tenochitlan (Dried Basin Under Mexico City)
+                <strong>Suspected Location:</strong> Lake Tauplitz, Austria
             </p>
             <br>
             <p>
-                <strong>Wikipedia Page:</strong> https://en.wikipedia.org/wiki/La_Noche_Triste
+                <strong>Wikipedia Page: </strong><a href = "https://en.wikipedia.org/wiki/Lake_Toplitz" target="_blank">https://en.wikipedia.org/wiki/Lake_Toplitz</a>
             </p>
             <br>
             <p>
-                <strong>Estimated Value:</strong> $500 Million
+                <strong>Estimated Value:</strong> $5.6 Billion
             </p>
             <br>
             <p>
-                <strong>Origin:</strong> Aztec
+                <strong>Origin:</strong> German
             </p>
         </div>
     </div>
@@ -160,27 +160,27 @@ function borneCard() {
     <div class="back">
         <div class="back-content">
             <h2>
-                <strong>The Treasure of La Noche Triste</strong>
+                <strong>The Treasure of Jean LaFitte</strong>
             </h2>
             <br>
             <p>
-                In June 1520, the Aztecs drove Hernan Cortes and the Spanish conquistadores out of the capital city of Tenochitlan. As the Spanish fled, they took with them vast amounts of Aztecan gold and treasure, much of which was lost during their escape in the depths of the Tenochitlan waterways.
+                Jean LaFitte was a privateer and smuggler living in Louisiana during the War of 1812. He was well known for his banditry and was an outlaw in the United States, however, he was eventually captured by the US Government and forced to choose between imprisonment or raiding only British Ships during the war. He was immensely successful, but for reasons unknown, decided to hide some of his spoils in the bayous of Louisiana.
             </p>
             <br>
             <p>
-                <strong>Suspected Location:</strong> Waters around Tenochitlan (Dried Basin Under Mexico City)
+                <strong>Suspected Location:</strong> The Area surrounding New Orleans, Louisiana
             </p>
             <br>
             <p>
-                <strong>Wikipedia Page:</strong> https://en.wikipedia.org/wiki/La_Noche_Triste
+                <strong>Wikipedia Page: </strong><a href = "https://en.wikipedia.org/wiki/Jean_Lafitte" target="_blank">https://en.wikipedia.org/wiki/Jean_Lafitte</a>
             </p>
             <br>
             <p>
-                <strong>Estimated Value:</strong> $500 Million
+                <strong>Estimated Value:</strong> $400 - $500 Million
             </p>
             <br>
             <p>
-                <strong>Origin:</strong> Aztec
+                <strong>Origin:</strong> British
             </p>
         </div>
     </div>
@@ -198,27 +198,27 @@ function cocasCard() {
     <div class="back">
         <div class="back-content">
             <h2>
-                <strong>The Treasure of La Noche Triste</strong>
+                <strong>Cocos Island Treasure</strong>
             </h2>
             <br>
             <p>
-                In June 1520, the Aztecs drove Hernan Cortes and the Spanish conquistadores out of the capital city of Tenochitlan. As the Spanish fled, they took with them vast amounts of Aztecan gold and treasure, much of which was lost during their escape in the depths of the Tenochitlan waterways.
+            In 1820, the Spain-Controlled Lima had to be evacuated due to mounting pressure coming from the South American wars of Independence. The Viceroy of Lima decided to transport the cities treasures to Mexico for safekeeping, but Captain William Thompson – the man hired to transport the treasure – absconded with it and hid it on Cocos Island off the coast of Costa Rica.
             </p>
             <br>
             <p>
-                <strong>Suspected Location:</strong> Waters around Tenochitlan (Dried Basin Under Mexico City)
+                <strong>Suspected Location:</strong> Cocos Island, Costa Rica
             </p>
             <br>
             <p>
-                <strong>Wikipedia Page:</strong> https://en.wikipedia.org/wiki/La_Noche_Triste
+                <strong>Wikipedia Page: </strong><a href = "https://en.wikipedia.org/wiki/Treasure_of_Lima" target="_blank">https://en.wikipedia.org/wiki/Treasure_of_Lima</a>
             </p>
             <br>
             <p>
-                <strong>Estimated Value:</strong> $500 Million
+                <strong>Estimated Value:</strong> Between $16 Million and $60 Million
             </p>
             <br>
             <p>
-                <strong>Origin:</strong> Aztec
+                <strong>Origin:</strong> Incan/Spanish
             </p>
         </div>
     </div>
